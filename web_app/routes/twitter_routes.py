c35da5ff5482c270db50100050314c3009acea3a
Martin Campbell <macostarz@gmail.com>
2020-06-16 21:18:41 -0500

    fix stuff

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,7 +1,7 @@
 # web_app/routes/twitter_routes.py
 
 from flask import Blueprint, render_template, jsonify
-from web_app.models import db, User, Tweet #, parse_records
+from web_app.models import db, User, Tweet  # parse_records
 from web_app.services.twitter_service import api as twitter_api_client
 from web_app.services.basilica_service import connection as basilica_api_client
 
